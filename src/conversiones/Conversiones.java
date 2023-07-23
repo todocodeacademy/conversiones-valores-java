db201dfceada81f70ec4f77e3da443166cd198e0
TodoCode <86254584+todocodeacademy@users.noreply.github.com>
2023-07-23 00:11:05 -0300

    Add files via upload

---
+++ src/conversiones/Conversiones.java
@@ -0,0 +1,25 @@
+package conversiones;
+
+public class Conversiones {
+   
+    public static void main(String[] args) {
+       /*String cantidad = "15";
+       String precio = "150.27";
+       
+       int cantEntero = Integer.parseInt(cantidad);
+       double precioDouble = Double.parseDouble(precio);*/
+       
+       int edad = 30;
+       double estatura = 1.67;
+       
+       String edadString = String.valueOf(edad);
+       String estaturaString = String.valueOf(estatura);
+       
+        System.out.println("Edad: " + edadString + " Estatura: " + estaturaString);
+       
+       
+        
+        
+    }
+    
+}
